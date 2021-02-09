bea13c0e8c33541d4bc50b2d1d31ade0f0938a94
Rhone Lachner <fernandclay@gmail.com>
2021-02-08 16:02:21 -0800

    added home

---
+++ src/components/containers/Details.jsx
@@ -0,0 +1,13 @@
+// import React, { Component, useState } from 'react';
+// import { getVillagerById } from '../villagers/villagerApi';
+// import Villager from '../villagers/Villager';
+
+// const Details extends Component {
+//   render() {
+//     return (
+//       <div>
+//           LATER      
+//       </div>
+//     );
+//   }
+// }
--- src/components/villagers/Villager.jsx
+++ src/components/villagers/Villager.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Villager = ({ _id, name, image, quote, skill }) => (
+const Villager = ({ _id, name, image, quote }) => (
   <figure>
     <Link to={`/details/${_id}`}>
       <img src={image} alt={name} />
     </Link>
     <figcaption>{name}</figcaption>
     <figcaption>{quote}</figcaption>
-    <figcaption>{skill}</figcaption>
   </figure>
 );
 
@@ -17,7 +16,6 @@ Villager.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   quote: PropTypes.string.isRequired,
-  skill: PropTypes.string.isRequired,
   _id: PropTypes.number.isRequired
 };
 
